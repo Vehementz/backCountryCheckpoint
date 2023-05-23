e8b0f18734d8e914d949d42e94e193bf9ada89af
Vehementz <vale-tudo91@sfr.fr>
2023-05-23 09:35:46 +0200

    first commit initialisation

--- src/entities/Post.ts
+++ src/entities/Post.ts
@@ -1,59 +1,59 @@
-import {
-  Entity,
-  Column,
-  PrimaryGeneratedColumn,
-  ManyToOne,
-  OneToMany,
-  OneToOne,
-  JoinColumn,
-} from "typeorm";
-import { ObjectType, Field, ID, InputType } from "type-graphql";
-import { Length } from "class-validator";
-import { User } from "./User";
-import { Comment } from "./Comment";
-import { Image } from "./Image";
-import { UniqueRelation } from "./common";
+// import {
+//   Entity,
+//   Column,
+//   PrimaryGeneratedColumn,
+//   ManyToOne,
+//   OneToMany,
+//   OneToOne,
+//   JoinColumn,
+// } from "typeorm";
+// import { ObjectType, Field, ID, InputType } from "type-graphql";
+// import { Length } from "class-validator";
+// import { User } from "./User";
+// import { Comment } from "./Comment";
+// import { Image } from "./Image";
+// import { UniqueRelation } from "./common";
 
-@Entity()
-@ObjectType()
-export class Post {
-  @PrimaryGeneratedColumn()
-  @Field(() => ID)
-  id: number;
+// @Entity()
+// @ObjectType()
+// export class Post {
+//   @PrimaryGeneratedColumn()
+//   @Field(() => ID)
+//   id: number;
 
-  @Column()
-  @Field()
-  content: string;
+//   @Column()
+//   @Field()
+//   content: string;
 
-  @OneToMany(() => Comment, (comment) => comment.post, { lazy: true })
-  @Field(() => [Comment])
-  comments: Promise<Comment[]>;
+//   @OneToMany(() => Comment, (comment) => comment.post, { lazy: true })
+//   @Field(() => [Comment])
+//   comments: Promise<Comment[]>;
 
-  @OneToOne(() => Image, (image) => image.post)
-  @JoinColumn()
-  @Field(() => Image)
-  image: Image;
+//   @OneToOne(() => Image, (image) => image.post)
+//   @JoinColumn()
+//   @Field(() => Image)
+//   image: Image;
 
-  @ManyToOne(() => User, { lazy: true })
-  @Field(() => User, { nullable: true })
-  createdBy: User;
+//   @ManyToOne(() => User, { lazy: true })
+//   @Field(() => User, { nullable: true })
+//   createdBy: User;
 
-  @Column()
-  @Field(() => Date)
-  createdAt: Date;
-}
+//   @Column()
+//   @Field(() => Date)
+//   createdAt: Date;
+// }
 
-@InputType()
-export class PostInput {
-  @Field()
-  @Length(5, 500)
-  content: string;
+// @InputType()
+// export class PostInput {
+//   @Field()
+//   @Length(5, 500)
+//   content: string;
 
-  // { id: number, url: string, ... }
-  @Field(() => UniqueRelation, { nullable: true })
-  image: UniqueRelation;
+//   // { id: number, url: string, ... }
+//   @Field(() => UniqueRelation, { nullable: true })
+//   image: UniqueRelation;
 
-  // these fields are not available for GraphQL
-  createdAt: Date;
-  createdBy: User;
-}
+//   // these fields are not available for GraphQL
+//   createdAt: Date;
+//   createdBy: User;
+// }
